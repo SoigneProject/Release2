caaf2113516e72ed5f2394b6ea73dbbd3f4bb9d1
Anthony Jhong <anthonyjhong@yahoo.com>
2019-11-10 18:06:40 -0800

    Added Get All Post with TAG Name

--- backend/api/routes/postRoute.js
+++ backend/api/routes/postRoute.js
@@ -171,12 +171,21 @@ router.put('/removeTag/:id', function (req, res) {
 })
 
 router.get('/AllPostsByTag/:tagName', function (req, res) {
-    console.log("All post by tag name");
+    // console.log("All post by tag name");
+    // var queryTagName = req.params.tagName;
+    // PostModel.find({ "tags.name": queryTagName}).then(function(tags) {
+    //     console.log(tags);
+    //     return res.status(200).json(tags);
+    // })
     var queryTagName = req.params.tagName;
-    PostModel.find({ "tags.name": queryTagName}).then(function(tags) {
-        console.log(tags);
-        return res.status(200).json(tags);
-    })
+    PostModel.find({'tags': { $elemMatch: { 'tagName': queryTagName}}},
+    function (err, obj) {
+        if (err) return res.json({
+            success: false,
+            error: err
+        });
+        return res.send(obj);
+    });
 })
 
 module.exports = router;
