9ff2e849d42416cb4da47895959a21f53c5af167
Sarah Supat <43248599+ssupat97@users.noreply.github.com>
2019-11-17 17:56:03 -0800

    Create PostCreationTest.java

---
+++ Test/PostCreationTest.java
@@ -0,0 +1,13 @@
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PostCreationTest {
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+  
+  }
+  
+}
