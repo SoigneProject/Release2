bd1c038cf0502ba9a7c2c599d200a31da30be32b
Leonel <44385935+LeoDAnda18@users.noreply.github.com>
2019-11-30 00:52:38 -0800

    Add files via upload
    turned MyFeed into a class component and made MyFeed Dynamic,

--- frontend/client/src/MyFeed.js
+++ frontend/client/src/MyFeed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -11,6 +11,7 @@ import Box from '@material-ui/core/Box';
 //import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 import Container from '@material-ui/core/Container';
 import ReactDOM from 'react-dom'
 import GridList from '@material-ui/core/GridList';
@@ -24,9 +25,154 @@ import pic4 from './images/4.jpeg';
 import pic5 from './images/5.jpeg';
 import TopMenu from './TopMenu';
 import PostPopup from './PostPopup';
+import axios from 'axios';
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
 
+class MyFeed extends Component
+{
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      userPosts: [
+        {
+          dateTime: "loading",
+          description: "temp",
+          photo: "temp",
+          tags: [],
+          title: "loading",
+          username: "temp"
+        }
+      ],
+      username: "temp",
+      following: [{username: "temp"}],
+      isAuthenticating: true,
+    }
+  }
 
+  
+  componentDidMount() {
+    // Retreive user data
+    axios
+      .get("http://localhost:6969/user/currentuser", { withCredentials: true })
+      .then(json => {
+        if (!json.data.username) {
+          this.props.history.push("/signModal");
+        } else {
+          axios
+            .get("http://localhost:6969/users/" + json.data.username)
+            .then(json => {
+              this.setState({ username: json.data.username, following: json.data.following, isAuthenticating: false });
+              this.state.userPosts.pop();
+              for(var i = 0; i < this.state.following.length; i++)
+              {
+                axios.get("http://localhost:6969/posts/username/" + this.state.following[i].username)
+                .then(json => {
+                  console.log(json);
+                  this.state.userPosts.push(json.data);
+                });
+              }
+              this.setState({userPosts: this.state.userPosts})
+            });
+        }
+      });
+  }
+
+  render()
+  {
+    if (this.state.isAuthenticating) return null;
+
+    const { userPosts} = this.state;
+
+    const theme = createMuiTheme({
+      '@global' : {
+        body: {
+          backgroundColor: "white",
+        }
+      }
+    });
+
+    const paperStyle = {
+      marginTop: 8,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }
+
+    const avatarStyle = {
+      margin: 1,
+      backgroundColor: "gray",
+    }
+
+    const formStyle = {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: 3,
+    }
+
+    const submitStyle = {
+      marginTop: 3,
+      marginBottom: 0,
+      marginRight: 2,
+    }
+
+    const tileStyle = {
+  
+    }
+  
+    const gridStyle = {
+      marginTop: 20,
+  
+    }
+
+    const titleStyle = {
+      marginTop: 20,
+    }
+
+    return (
+      <div>
+      <TopMenu></TopMenu>
+      <Grid container spacing={3}>
+      <Grid item xs={6}>
+      <Typography style = {titleStyle} align = 'Left' variant="h3" component="h2" >
+      My Feed</Typography>
+      </Grid>
+      <Grid item xs = {6}>
+      <Button style = {{marginTop: 28, padding: 10, marginRight: 40, float: 'right'}} variant="contained" color="secondary" className="button">
+      Create Post
+    </Button>
+    </Grid>
+    </Grid>
+      <Grid container spacing={3}>
+      <Grid item xs={12}>
+      <GridList cols = {4} spacing={5} style = {gridStyle} cellHeight={500} >
+  
+      {userPosts.map(post => (
+        <GridListTile style = {tileStyle} key={post.title}>
+          <img src={post.photo} alt={post.photo} />
+          <GridListTileBar
+            title={post.title}
+            subtitle={<span>by: {post.username}</span>}
+            actionIcon={
+              <PostPopup></PostPopup>
+            }
+          />
+        </GridListTile>
+      ))}
+    </GridList>
+    
+      </Grid>
+      
+    
+    </Grid>
+    </div>
+  
+    );
+  }
+}
+
+export default MyFeed;
+
+/*
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
@@ -184,3 +330,4 @@ export default function Feed() {
 
   );
 }
+*/
