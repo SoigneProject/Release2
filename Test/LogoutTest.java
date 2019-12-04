510442d3e9ba2a66742c50a6815dc0dc283134f7
Sarah Supat <43248599+ssupat97@users.noreply.github.com>
2019-12-04 01:43:42 -0800

    Update LogoutTest.java

--- Test/LogoutTest.java
+++ Test/LogoutTest.java
@@ -8,30 +8,45 @@ public class LogoutTest {
 
 	public static void main(String[] args) {
 		//Set the driver properties.
-		System.setProperty("webdriver.chrome.driver", "add your own local path to the webdriver");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sssup\\OneDrive\\Documents\\Soigne Test\\chromedriver_win32\\chromedriver.exe");
 
 		//Open the Chrome browser.
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 
 		//Maximize the browser window.
 		driver.manage().window().maximize();
 
-		if (testPostCreation()) {
+		//Navigate the driver to the user's profile page.
+		driver.navigate().to("http://localhost:3000/");
 
+		//Wait for the element to load.
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+		//Tests the Logout button.
+		if (testLogout()) {
+			System.out.println("Logout Test: Successful");
 		}
 		else {
-
+			System.out.println("Logout Test: Failed");
 		}
-
+		
 		driver.close();
 		driver.quit();
 	}
 
-	/**
-	 * Test:
-	 * @return boolean
-	 */
 	private static boolean testLogout() {
-		return false;
+		try {
+			//Click on the Logout button.
+			driver.findElement(By.cssSelector("#root > div > div > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-3 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-4 > div:nth-child(2) > button")).click();
+
+			//Wait for the element to load.
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+			return true;
+		}
+		catch (final Exception e) {
+			System.out.println(e.getClass().toString());
+			return false;
+		}
 	}
 }
