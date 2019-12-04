fe049227e5f2ced627cfa70ef2c230f780db1598
Sarah Supat <43248599+ssupat97@users.noreply.github.com>
2019-12-04 01:53:04 -0800

    Update SearchBarTest.java

--- Test/SearchBarTest.java
+++ Test/SearchBarTest.java
@@ -8,30 +8,136 @@ public class SearchBarTest {
 
 	public static void main(String[] args) {
 		//Set the driver properties.
-		System.setProperty("webdriver.chrome.driver", "add your own local path to the webdriver");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sssup\\OneDrive\\Documents\\Soigne Test\\chromedriver_win32\\chromedriver.exe");
 
 		//Open the Chrome browser.
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 
 		//Maximize the browser window.
 		driver.manage().window().maximize();
 
-		if (testPostCreation()) {
+		//Navigate the driver to the website's URL.
+		driver.navigate().to("http://localhost:3000/CreatePost");
+		
+		//Wait for the element to load.
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
+		/**
+		 * Tests the Search Bar for Users.
+		 */
+		if (testSearchUsers()) {
+			System.out.println("Search Bar for Users: Successful");
 		}
 		else {
-
+			System.out.println("Search Bar for Users: Failed");
 		}
-
+		
+		/**
+		 * Tests the Search Bar for Posts.
+		 */
+		if (testSearchPosts()) {
+			System.out.println("Search Bar for Posts Test: Successful");
+		}
+		else {
+			System.out.println("Search Bar for Posts Test: Failed");
+		}/**
+		 * Tests the Search Bar for Items.
+		 */
+		if (testSearchItems()) {
+			System.out.println("Search Bar for Items: Successful");
+		}
+		else {
+			System.out.println("Search Bar for Items: Failed");
+		}
+		
 		driver.close();
 		driver.quit();
 	}
 
 	/**
-	 * Test:
-	 * @return boolean
+	 * Test: Search Bar for Users
+	 * @return boolean 
 	 */
-	private static boolean testSearchBar() {
-		return false;
+	private static boolean testSearchUsers() {
+		try {
+			//Enter the Title.
+			driver.findElement(By.cssSelector("#name")).sendKeys("");
+
+			//Click the Add Item Button.
+			driver.findElement(By.cssSelector("#root > div > div > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-3 > div:nth-child(3) > div > button")).click();;
+			
+			//Wait for page to load.
+			Thread.sleep(5000);
+			
+			/**if (driver.findElement(By.tagName("h1")).getText().equals("")) {
+				return true;
+			}
+			else {
+				return false;
+			}**/
+			return true;
+		}
+		catch (final Exception e) {
+			System.out.println(e.getClass().toString());
+			return false;			
+		}
+	}
+	
+	/**
+	 * Test: Search Bar for Posts
+	 * @return boolean 
+	 */
+	private static boolean testSearchPosts() {
+		try {
+			//Enter the Title.
+			driver.findElement(By.cssSelector("#name")).sendKeys("");
+
+			//Click the Add Item Button.
+			driver.findElement(By.cssSelector("#root > div > div > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-3 > div:nth-child(3) > div > button")).click();;
+			
+			//Wait for page to load.
+			Thread.sleep(5000);
+			
+			/**if (driver.findElement(By.tagName("h1")).getText().equals("")) {
+				return true;
+			}
+			else {
+				return false;
+			}**/
+			return true;
+		}
+		catch (final Exception e) {
+			System.out.println(e.getClass().toString());
+			return false;			
+		}
+	}
+	
+	/**
+	 * Test: Search Bar for Items
+	 * @return boolean 
+	 */
+	private static boolean testSearchItems() {
+		try {
+			//Enter the Title.
+			driver.findElement(By.cssSelector("#name")).sendKeys("");
+
+			//Click the Add Item Button.
+			driver.findElement(By.cssSelector("#root > div > div > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-3 > div:nth-child(3) > div > button")).click();;
+			
+			//Wait for page to load.
+			Thread.sleep(5000);
+			
+			/**if (driver.findElement(By.tagName("h1")).getText().equals("")) {
+				return true;
+			}
+			else {
+				return false;
+			}**/
+			return true;
+		}
+		catch (final Exception e) {
+			System.out.println(e.getClass().toString());
+			return false;			
+		}
 	}
 }
